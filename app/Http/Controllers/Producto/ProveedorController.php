e8e69f9c4796a998fd6053510d303b6c1caa51de
josericardo123 <josericardocauichmis@gmail.com>
2021-10-25 17:42:43 -0500

    Cambios nuevos

--- app/Http/Controllers/Producto/ProveedorController.php
+++ app/Http/Controllers/Producto/ProveedorController.php
@@ -8,6 +8,13 @@ use App\Models\Cruds\Proveedor;
 
 class ProveedorController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('can:admin.proveedores.index')->only('index');
+        $this->middleware('can:admin.proveedores.create')->only('create', 'store');
+        $this->middleware('can:admin.proveedores.edit')->only('edit', 'update');
+        $this->middleware('can:admin.proveedores.destroy')->only('destroy');
+    }
     public function index()
     {
         return view('cruds.proveedores.index');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Bievenido al sistema administrativo de <b>GRUPO TIKAL</b>.</p>
 @stop
 
 {{--@section('css')
